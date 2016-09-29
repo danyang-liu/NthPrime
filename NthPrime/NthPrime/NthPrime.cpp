940087125529cdd3422ec212cc09587b7e301a92
v-danyl <v-danyl@microsoft.com>
2016-09-29 15:50:49 +0800

    youhua

--- NthPrime/NthPrime/NthPrime.cpp
+++ NthPrime/NthPrime/NthPrime.cpp
@@ -1,64 +1,56 @@
-﻿#include"stdafx.h"
+﻿#include "stdafx.h"
 #include <iostream>
-#include <fstream>
-#include <string>
 #include <vector>
-#include <algorithm>
-#include <cmath>
+#include<fstream>
+#include <time.h>
+#define MAXSIZE 100000000
+int isNotPrime[MAXSIZE];
+int prime[MAXSIZE];
 using namespace std;
-
-int getnthprime(int n)
+/*
+const long N = 2000;
+long prime[N] = { 0 }, num_prime = 0;
+int isNotPrime[N] = { 1, 1 };
+*/
+int main()
 {
-	int upper = 1000;
-	if (n > 100)
-	{
-		upper = 1.2 * n*log(n);
-	}
-	vector<int>primes;
-	for (int i = 0; i <= upper; ++i)
-	{
-		primes.push_back(1);   //向量元素值若为1，则元素序数为质数；向量元素若为0，则元素序数不是质数。初始时假设所有数皆为质数。
-	}
-	primes[0] = 0;
-	primes[1] = 0;      ////0,1都不是质数
-	for (int i = 2; i <= sqrt(upper); ++i)
-	{
-		if (primes[i] == 1)
-		{
-			for (int multiple = 2; i*multiple <= upper; ++multiple)
-				primes[i*multiple] = 0;
-		}
-	}
-	int index = 0;
-	int k = 0;
-	while (index<n)
-	{
-		if (primes[k] == 1)
-		{
-			index++;
-			k++;
-		}
-		else
-		{
-			k++;
-		}
-	}
-	k--;
-	return k;
-}
-
-int main(int argc, const char * argv[]) {
 	ofstream fout;
 	fout.open("result.txt", ios::out);
 	cout << "please enter n:(enter 0 end):" << endl;
 	int n;
 	cin >> n;
-	while (n > 0)
+
+	int upper = 1000;
+	if (n > 100)
 	{
-		fout << getnthprime(n) << endl;
-		cout << "please enter n:(enter 0 end):" << endl;
-		cin >> n;
+		upper = 1.3 * n*log(n);
 	}
-	fout.close();
+
+	int num_prime = 0;
+
+	memset(isNotPrime, 0, sizeof(int)*upper);
+	memset(prime, 0, sizeof(int)*n);
+	isNotPrime[0] = 1;
+	isNotPrime[1] = 1;
+	long beginTime = clock();//获得开始时间，单位为毫秒  
+	for (int i = 2; i < upper; i++)
+	{
+		if (!isNotPrime[i])
+		{
+			prime[num_prime++] = i;
+		}
+		for (int j = 0; j < num_prime && i * prime[j] < upper; j++)
+		{
+			isNotPrime[i * prime[j]] = 1;
+			if (!(i % prime[j]))
+				break;
+		}
+	}
+	fout<<"the"<<n<<"th prime:" << prime[n - 1] << endl;
+	//	cout << prime[1000000];
+	long endTime = clock();//获得结束时间
+	fout << "beginTime:" << beginTime << endl
+		<< "endTime:" << endTime << endl
+		<< "endTime-beginTime:" << endTime - beginTime << "ms" << endl;
 	return 0;
 }
